6e251bf3c7066e81013967a4a35f5dcf5b2869cc
Israel David Dahan <id@studiodvd.co.il>
2024-01-04 18:25:10 +0200

    authorized

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,21 +4,15 @@ import jwt from 'jsonwebtoken'
 
 const auth = async (req, res, next) => {
     let token = req.headers['authorization']
-    if (!token) {
-        throw new Error('No token provided')
-    }
     try {
         token = token.replace('Bearer ', '')
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const { id } = decoded
         const user = await User.findOne({ where: { id } })
-        if (!user) {
-            throw new Error('Invalid token')
-        }
         req.user = user
         req.token = token
     } catch (err) {
-        throw new Error('Invalid token')
+        return res.status(401).send({ error: 'Not authorized to access this resource' })
     }
 
     
