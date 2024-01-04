25d6f4ef5edc4cc83216f4475255d5fbb16363b4
Israel David Dahan <id@studiodvd.co.il>
2024-01-04 18:20:57 +0200

    disable force

--- database/index.js
+++ database/index.js
@@ -9,7 +9,7 @@ const syncModels = async () => {
     try {
         await sequelize.authenticate();
         console.log("Connection has been established successfully.");
-        await sequelize.sync({ force: true });
+        await sequelize.sync();
         console.log("All models were synchronized successfully.");
     } catch (error) {
         console.error("Unable to connect to the database:", error);
